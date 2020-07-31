27966933dc4a82bb93fb3660e70cb485355c9e21
Ricardo van Noort <ricardo.van.noort@hva.nl>
2020-07-31 17:18:23 +0200

    Added recruiter's view and made login dependant on user type

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('profile/', views.profile, name='profile'),
+    path('profile/', views.login_success, name='profile'),
+    path('candidate-profile/', views.candidate_profile, name='candidate_profile'),
+    path('recruiter-profile/', views.recruiter_profile, name='recruiter_profile'),
 
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -7,8 +7,18 @@ from .forms import UserForm, JobSeekerProfileForm, RecruiterProfileForm
 
 
 @login_required
-def profile(request):
-    """ Display the user's profile. """
+def login_success(request):
+    """ Display user's profile. """
+
+    if request.user.is_job_seeker:
+        return redirect('candidate_profile')
+    else:
+        return redirect('recruiter_profile')
+
+
+@ login_required
+def candidate_profile(request):
+    """ Display job seeker's profile. """
 
     if request.method == 'POST':
         user_form = UserForm(request.POST, instance=request.user)
@@ -38,10 +48,61 @@ def profile(request):
         profile_form = JobSeekerProfileForm(
             instance=request.user.jobseekerprofile)
 
-    template = 'profiles/user_profile.html'
+    first_name = request.user.first_name
+    template = 'profiles/candidate_profile.html'
     context = {
         'user_form': user_form,
         'profile_form': profile_form,
+        'first_name': first_name,
+    }
+
+    return render(request, template, context)
+
+
+@ login_required
+def recruiter_profile(request):
+    """ Display recruiter's profile. """
+
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = RecruiterProfileForm(
+            request.POST, instance=request.user.recruiterprofile)
+        if user_form.is_valid() and profile_form.is_valid():
+            forms = user_form.save(commit=False)
+            forms.save()
+            forms.recruiterprofile.company_name = profile_form.cleaned_data.get(
+                'company_name')
+            forms.recruiterprofile.company_address1 = profile_form.cleaned_data.get(
+                'company_address1')
+            forms.recruiterprofile.company_address2 = profile_form.cleaned_data.get(
+                'company_address2')
+            forms.recruiterprofile.company_city = profile_form.cleaned_data.get(
+                'company_city')
+            forms.recruiterprofile.company_ZIP = profile_form.cleaned_data.get(
+                'company_ZIP')
+            forms.recruiterprofile.company_state = profile_form.cleaned_data.get(
+                'company_state')
+            forms.recruiterprofile.company_country = profile_form.cleaned_data.get(
+                'company_country')
+
+            forms.recruiterprofile.save()
+            messages.success(request, 'Profile updated successfully')
+            return redirect(reverse('view_home'))
+        else:
+            messages.error(request,
+                           ('Update failed. Please ensure '
+                            'the form is valid.'))
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = RecruiterProfileForm(
+            instance=request.user.recruiterprofile)
+
+    first_name = request.user.first_name
+    template = 'profiles/recruiter_profile.html'
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form,
+        'first_name': first_name,
     }
 
     return render(request, template, context)
