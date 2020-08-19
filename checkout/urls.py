33a3ab4b268a7ef8c837dc3ab28c9b6695272121
Ricardo van Noort <ricardo.van.noort@hva.nl>
2020-08-19 19:46:40 +0200

    Added confirmation and recruiterprofile activation

--- checkout/urls.py
+++ checkout/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('payment/', views.payment, name='payment'),
+    path('confirmation/<subscription_id>',
+         views.confirmation, name='confirmation'),
 
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, reverse
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from profiles.models import User
+from profiles.models import User, RecruiterProfile
 from plans.models import PlanSubscription, Plans
 import stripe
 
@@ -14,6 +14,14 @@ def get_user(request):
     return None
 
 
+def get_recruiter_profile(request):
+    recruiter_profile = RecruiterProfile.objects.filter(user=request.user)
+    if recruiter_profile.exists():
+        return recruiter_profile.first()
+    return None
+
+
+@login_required
 def payment(request):
     publishable_key = settings.STRIPE_PUBLISHABLE_KEY
 
@@ -35,11 +43,42 @@ def payment(request):
             ]
         )
 
-        return redirect(reverse('view_home'))
+        return redirect(reverse('confirmation',
+                                kwargs={
+                                    'subscription_id': subscription.id
+                                }))
 
     template = 'checkout/checkout.html'
     context = {
         'publishable_key': publishable_key
+
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def confirmation(request, subscription_id):
+    user = get_user(request)
+    pricing_plan = Plans.objects.filter(plan_type='Recruiter plan').first()
+    user.is_job_seeker = False
+    user.plan_type = pricing_plan
+    user.save()
+
+    recruiter_profile = get_recruiter_profile(request)
+    plan_subscription, created = PlanSubscription.objects.get_or_create(
+        recruiter_profile=recruiter_profile)
+    plan_subscription.stripe_subscription_id = subscription_id
+    plan_subscription.active = True
+    plan_subscription.save()
+
+    messages.success(
+        request, 'Thank you for your recurrent payment! You are now able to post your developer vacancies.')
+
+    template = 'checkout/confirmation.html'
+    context = {
+        'subscription_id': subscription_id,
+        'first_name': user.first_name,
 
     }
 
