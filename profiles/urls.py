1783ab997f0f9f3d0d5e7131be1708814fec82fb
Ricardo van Noort <ricardo.van.noort@hva.nl>
2020-08-20 18:29:00 +0200

    Activated recruiter profile based on payment

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('profile/', views.login_success, name='profile'),
     path('candidate-profile/', views.candidate_profile, name='candidate_profile'),
-    #path('recruiter-profile/', views.recruiter_profile, name='recruiter_profile'),
+    path('recruiter-profile/', views.recruiter_profile, name='recruiter_profile'),
     path('candidate-profile/edit-candidate-profile/', views.edit_candidate_profile,
          name='edit_candidate_profile'),
     path('candidate-profile/edit-work-experience/', views.edit_work_experience,
--- profiles/views.py
+++ profiles/views.py
@@ -11,9 +11,9 @@ def login_success(request):
     """ Display user's profile. """
 
     if request.user.is_job_seeker:
-        return redirect('candidate_profile')
+        return redirect(reverse('candidate_profile'))
     else:
-        return redirect('recruiter_profile')
+        return redirect(reverse('recruiter_profile'))
 
 
 @ login_required
@@ -171,63 +171,15 @@ def delete_education(request, education_id):
         return redirect(reverse('edit_education'))
 
 
-# def language_list(request):
-#     language_list = Languages.objects.all()
+@ login_required
+def recruiter_profile(request):
+    """ Display recruiter's profile. """
 
-#     template = 'profiles/languages.html'
-#     context = {
-#         'language_list': language_list,
+    user = get_object_or_404(User, id=request.user.id)
 
-#     }
+    template = 'profiles/recruiter_profile.html'
+    context = {
+        'user': user,
+    }
 
-#     return render(request, template, context)
-
-# @ login_required
-# def recruiter_profile(request):
-#     """ Display recruiter's profile. """
-
-#     if request.method == 'POST':
-#         user_form = UserForm(request.POST,
-#                              request.FILES,
-#                              instance=request.user)
-#         profile_form = RecruiterProfileForm(
-#             request.POST, instance=request.user.recruiterprofile)
-#         if user_form.is_valid() and profile_form.is_valid():
-#             forms = user_form.save(commit=False)
-#             forms.save()
-#             forms.recruiterprofile.company_name = profile_form.cleaned_data.get(
-#                 'company_name')
-#             forms.recruiterprofile.company_address1 = profile_form.cleaned_data.get(
-#                 'company_address1')
-#             forms.recruiterprofile.company_address2 = profile_form.cleaned_data.get(
-#                 'company_address2')
-#             forms.recruiterprofile.company_city = profile_form.cleaned_data.get(
-#                 'company_city')
-#             forms.recruiterprofile.company_ZIP = profile_form.cleaned_data.get(
-#                 'company_ZIP')
-#             forms.recruiterprofile.company_state = profile_form.cleaned_data.get(
-#                 'company_state')
-#             forms.recruiterprofile.company_country = profile_form.cleaned_data.get(
-#                 'company_country')
-
-#             forms.recruiterprofile.save()
-#             messages.success(request, 'Profile updated successfully')
-#             return redirect(reverse('view_home'))
-#         else:
-#             messages.error(request,
-#                            ('Update failed. Please ensure '
-#                             'the form is valid.'))
-#     else:
-#         user_form = UserForm(instance=request.user)
-#         profile_form = RecruiterProfileForm(
-#             instance=request.user.recruiterprofile)
-
-#     first_name = request.user.first_name
-#     template = 'profiles/recruiter_profile.html'
-#     context = {
-#         'user_form': user_form,
-#         'profile_form': profile_form,
-#         'first_name': first_name,
-#     }
-
-#     return render(request, template, context)
+    return render(request, template, context)
