f73686c9ba021e7d776f66c98da456425ee38e6d
Ricardo van Noort <ricardo.van.noort@hva.nl>
2020-09-20 14:05:30 +0200

    Changed description into TextField

--- jobs/models.py
+++ jobs/models.py
@@ -10,7 +10,7 @@ class Jobs(models.Model):
         verbose_name_plural = 'Jobs'
 
     title = models.CharField(max_length=50)
-    description = models.CharField(max_length=1024)
+    description = models.TextField(max_length=1024)
     # Delete job when user is deleted
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
